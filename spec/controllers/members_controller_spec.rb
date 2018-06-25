dffc4be5aa8db909846b285954fe32f16c7c5b4d
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-06-25 16:25:39 -0300

    add Member controller Test

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,25 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
+    @campaign = FactoryBot.create(:campaign, user: @current_user)
+  end
 
   describe "GET #create" do
+    before(:each) do
+      @new_member_attributes = attributes_for(:member, campaign_id: @campaign.id)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
     it "returns http success" do
-      get :create
+      post :create, params: {member: @new_member_attributes}
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #destroy" do
+  describe "DELETE #destroy" do
+    before(:each) do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
     it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+        member = create(:member, campaign: @campaign)
+        delete :destroy, params: {id: member.id}
+        expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #update" do
+  describe "PUT #update" do
+    before(:each) do
+      @new_member_attributes = attributes_for(:member, campaign_id: @campaign.id)
+      member = create(:member, campaign_id: @campaign.id)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+      put :update, params: {id: member.id, member: @new_member_attributes}
+    end
+   
     it "returns http success" do
-      get :update
       expect(response).to have_http_status(:success)
+    end
+
+    it "Campaign have the new attributes" do
+      expect(Member.last.name).to eq(@new_member_attributes[:name])
+      expect(Member.last.email).to eq(@new_member_attributes[:email])
+    end
+  end
+
+  describe "PUT #opened" do
+    before(:each) do
+      @member = create(:member, campaign_id: @campaign.id)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+      @member.set_pixel
+    end
+   
+    it "Member is not opened when set pixel" do
+      @member.reload
+      expect(Member.last.open).to be(false)
+    end
+
+    it "Member is opened with the valid token" do
+      get :opened, params: {token: @member.token}
+      @member.reload
+      expect(Member.last.open).to be_truthy
     end
   end
 
